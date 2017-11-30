a50fae7633bcfa761a53e9515b8a4f4bab3511e4
valeriorisuleo <valerio.risuleo@gmail.com>
2017-11-30 01:36:48 +0000

    qbit test REFACT

--- QuBit_Test/Answer.js
+++ QuBit_Test/Answer.js
@@ -6,21 +6,21 @@
     var elements = [];
 
 // ANSWER B
-    if (mySelector.includes('.') && mySelector.includes('img')) {
+    if (mySelector.indexOf('.') > -1 && mySelector.indexOf('img') > -1) {
       for (var z = 0; z < all.length; z++) {
         if (all[z].className !== '' && all[z].tagName === 'IMG') {
           elements.push(all[z]);
         }
       }
 // ANSWER C, F, G
-    } else if (mySelector.includes('#')) {
+    } else if (mySelector.indexOf('#') > -1) {
       for (var y = 0; y < all.length; y++) {
         if (all[y].id !== '' && all[y].tagName === 'DIV') {
           elements.push(all[y]);
         }
       }
 // ANSWER D
-    } else if (mySelector.includes('.')) {
+    } else if (mySelector.indexOf('.') > -1) {
       for (var i = 0; i < all.length; i++) {
         if (all[i].className !== '') {
           elements.push(all[i]);
@@ -35,4 +35,48 @@
     return elements;
   };
 
-  $();
+  $('');
+
+  // I'm pretty sure that IE9 doesn't suppoert .includes So I used ´mySelector.indexOf('.') > -1 `
+
+
+// REFACTORING
+
+  const all = document.getElementsByTagName('*');
+
+  const elements = [];
+  // I have 3 for loops inside here so ideally I make it more modular and make a function that takes some arguments and executes the loop for me.
+
+  // Argument structure array would be the 'all' which is document.getElementsByTagName('*');
+  // the selectorObject would be plain javascript object with the key to be the selector and the value to be the expected value so in example
+  // { tagName: 'DIV' } would return element['tagName'] === 'DIV'
+
+  const lardhouse = {
+    tagName: 'DIV'
+  };
+
+// we pass 2 arguments: the selectorObject and element
+  function isCorrect(selectorObject, element) { // element return {tagName: "DIV"}
+    // console.log('element', element);
+    // console.log('const all and selectorObject are the same!', selectorObject);
+    var filters = Object.keys(lardhouse); // this return ["tagName"]
+    // console.log('filters', filters);
+    return filters.every(function(filter){ // returns true if all cases returns true in the array loop
+      // console.log('filter', filter); //tagName
+      // console.log(element[filter]); // this returns DIV
+      // console.log(element[filter] === selectorObject[filter]);
+      return element[filter] === selectorObject[filter];
+    });
+  }
+
+  console.log('wow', isCorrect(all, lardhouse));
+
+  function sortElementsTocorrectCategory(array, selectorObject) {
+    array.forEach(function(element){
+      if(isCorrect(selectorObject, element)) { // returns true if it matches the correct elements
+        elements.push(all);
+      }
+    });
+  }
+
+  console.log('mama', sortElementsTocorrectCategory());
