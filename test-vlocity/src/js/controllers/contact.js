90ceb90df75b2f1b93828cca5edb20df4ecf40ae
valeriorisuleo <valerio.risuleo@gmail.com>
2017-11-30 18:55:06 +0000

    filterFilter in the contrller: done

--- test-vlocity/src/js/controllers/contact.js
+++ test-vlocity/src/js/controllers/contact.js
@@ -3,15 +3,20 @@ angular
 .controller('MainCtrl', MainCtrl);
 
 
-MainCtrl.$inject = ['ContactFact'];
-function MainCtrl(ContactFact) {
+MainCtrl.$inject = ['ContactFact', 'filterFilter', '$scope'];
+function MainCtrl(ContactFact, filterFilter, $scope) {
   const vm = this;
 
 // here we simulate a $http request to a server using the factories.
   vm.all = ContactFact.query();
 
+  function filterContact() {
+    vm.filtered = filterFilter(vm.all, vm.q); // add this instead of main.all
+  }
 
-  // function displaySelectedUser() {
-  //
-  // }
+  // filterContact();
+  // we call it only once. We need something to watch the changes in the controllers properties -> $scope
+
+  $scope.$watch(() => vm.q, filterContact);
+  // $scope.$watch('q', filterContact);
 }
