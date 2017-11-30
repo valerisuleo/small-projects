8ffbe19865c6c167058e8996effcfd16be163598
valeriorisuleo <valerio.risuleo@gmail.com>
2017-11-30 10:49:53 +0000

    can't wrap with if statement. Move to the body root..

--- QuBit_Test/Answer.js
+++ QuBit_Test/Answer.js
@@ -1,50 +1,54 @@
-  var $ = function (selector) {
-    const all = document.getElementsByTagName('*');
-    const divs = document.getElementsByTagName('div');
-
-    var mySelector = selector;
-    var elements = [];
-
-// ANSWER B
-    if (mySelector.indexOf('.') > -1 && mySelector.indexOf('img') > -1) {
-      for (var z = 0; z < all.length; z++) {
-        if (all[z].className !== '' && all[z].tagName === 'IMG') {
-          elements.push(all[z]);
-        }
-      }
-// ANSWER C, F, G
-    } else if (mySelector.indexOf('#') > -1) {
-      for (var y = 0; y < all.length; y++) {
-        if (all[y].id !== '' && all[y].tagName === 'DIV') {
-          elements.push(all[y]);
-        }
-      }
-// ANSWER D
-    } else if (mySelector.indexOf('.') > -1) {
-      for (var i = 0; i < all.length; i++) {
-        if (all[i].className !== '') {
-          elements.push(all[i]);
-        }
-      }
-// ANSWER A
-    } else if (mySelector === 'div') {
-      elements = divs;
-    }
-
-    console.log('elements', elements);
-    return elements;
-  };
-
-  $('');
+//   var $ = function (selector) {
+//     const all = document.getElementsByTagName('*');
+//     const divs = document.getElementsByTagName('div');
+//
+//     var mySelector = selector;
+//     var elements = [];
+//
+// // ANSWER B
+//     if (mySelector.indexOf('.') > -1 && mySelector.indexOf('img') > -1) {
+//       for (var z = 0; z < all.length; z++) {
+//         if (all[z].className !== '' && all[z].tagName === 'IMG') {
+//           elements.push(all[z]);
+//         }
+//       }
+// // ANSWER C, F, G
+//     } else if (mySelector.indexOf('#') > -1) {
+//       for (var y = 0; y < all.length; y++) {
+//         if (all[y].id !== '' && all[y].tagName === 'DIV') {
+//           elements.push(all[y]);
+//         }
+//       }
+// // ANSWER D
+//     } else if (mySelector.indexOf('.') > -1) {
+//       for (var i = 0; i < all.length; i++) {
+//         if (all[i].className !== '') {
+//           elements.push(all[i]);
+//         }
+//       }
+// // ANSWER A
+//     } else if (mySelector === 'div') {
+//       elements = divs;
+//     }
+//
+//     console.log('elements', elements);
+//     return elements;
+//   };
+//
+//   $('');
 
   // I'm pretty sure that IE9 doesn't suppoert .includes So I used ´mySelector.indexOf('.') > -1 `
 
 
-// REFACTORING
 
+
+// _________________________________REFACTORING_________________________________
+
+var $ = function (selector) {
   const all = document.getElementsByTagName('*');
 
-  const elements = [];
+  var mySelector = selector;
+  var elements = [];
   // I have 3 for loops inside here so ideally I make it more modular and make a function that takes some arguments and executes the loop for me.
 
   // Argument structure array would be the 'all' which is document.getElementsByTagName('*');
@@ -55,28 +59,38 @@
     tagName: 'DIV'
   };
 
-// we pass 2 arguments: the selectorObject and element
-  function isCorrect(selectorObject, element) { // element return {tagName: "DIV"}
-    // console.log('element', element);
-    // console.log('const all and selectorObject are the same!', selectorObject);
-    var filters = Object.keys(lardhouse); // this return ["tagName"]
-    // console.log('filters', filters);
-    return filters.every(function(filter){ // returns true if all cases returns true in the array loop
-      // console.log('filter', filter); //tagName
-      // console.log(element[filter]); // this returns DIV
-      // console.log(element[filter] === selectorObject[filter]);
-      return element[filter] === selectorObject[filter];
-    });
+  if (true) {
+    // we pass 2 arguments: the selectorObject and element
+    function isCorrect(selectorObject, element) {
+      // console.log('element', element); // element return {tagName: "DIV"}
+      // console.log('const all and selectorObject are the same!', selectorObject);
+      var filters = Object.keys(lardhouse);
+      // console.log('filters', filters); // this return ["tagName"]
+      return filters.every(function(filter){ // returns true if all cases returns true in the array loop
+        // console.log('filter', filter); //tagName
+        // console.log(element[filter]); // this returns DIV
+        // console.log(element[filter] === selectorObject[filter]);
+        return element[filter] === selectorObject[filter];
+      });
+    }
+
+    console.log('wow', isCorrect(all, lardhouse));
+
+    function sortElementsTocorrectCategory(all, selectorObject) {
+      console.log(all); // see what it is in here
+      all.forEach(function(element){
+        if(isCorrect(selectorObject, element)) { // returns true if it matches the correct elements
+          elements.push(all);
+        }
+      });
+    }
+    console.log(sortElementsTocorrectCategory(all, lardhouse));
   }
 
-  console.log('wow', isCorrect(all, lardhouse));
 
-  function sortElementsTocorrectCategory(array, selectorObject) {
-    array.forEach(function(element){
-      if(isCorrect(selectorObject, element)) { // returns true if it matches the correct elements
-        elements.push(all);
-      }
-    });
-  }
 
-  console.log('mama', sortElementsTocorrectCategory());
+  console.log('elements', elements);
+  return elements;
+};
+
+$('');
