61f937d77e4ddbaa3ad260c2cacbc18095330ab9
valeriorisuleo <valerio.risuleo@gmail.com>
2017-12-08 00:29:48 +0000

    desktop navbar done

--- test-tangent/src/js/controllers/nails.js
+++ test-tangent/src/js/controllers/nails.js
@@ -2,8 +2,8 @@ angular
 .module('tangent-test')
 .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['NailsFactory'];
-function MainCtrl(NailsFactory) {
+MainCtrl.$inject = ['NailsFactory', '$window'];
+function MainCtrl(NailsFactory, $window) {
   const vm = this;
 
   vm.all = NailsFactory.query();
@@ -13,11 +13,27 @@ function MainCtrl(NailsFactory) {
   vm.noWrapSlides = false; // This will decide whether or not the carousel is 'infinite' or not, i.e whether you can keep going round in a loop with the arrow buttons
   vm.active = 0; // This decides which slide is shown first (based on it's index in the array of slides)
 
-// If we update vm.active = 0 to be vm.active = 3, this will change which bird the carousel starts on when the page loads.
-// If we update vm.myInterval = 5000 to be vm.myInterval = 2000 the delay between the slides will be 2 seconds rather than 5.
-// If we update vm.noWrapSlides = false to be vm.noWrapSlides = true, once we hit the last slide we can't loop back to the first.
-// Toggle mobile navbar
 
+  angular.element($window);
+  console.log(window);
+
+  $window.onscroll = function() {
+    const dollarHeader = document.getElementById('dollar-header');
+    console.log(dollarHeader);
+    var scrollPos = document.documentElement.scrollTop;
+    console.log(scrollPos);
+    var mydivpos = document.getElementById('info').offsetTop;
+    // console.log(mydivpos);
+    if(scrollPos >= mydivpos) {
+      // console.log('fire!');
+      dollarHeader.classList.add('opaque');
+    } else {
+      dollarHeader.classList.remove('opaque');
+    }
+  };
+
+
+// Toggle mobile navbar
   vm.mobileNavBar = function () {
     vm.mobileOpen = !vm.mobileOpen;
   };
